3fbb6cc27eb9b30a6a90bbe15662bb080f91999c
Bordless <38050217+Bordless-ita@users.noreply.github.com>
2023-10-30 13:26:27 +0100

    Aggiornato
    Aggiornato per migliorare la compatibilità con server strutturati in maniera differente

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,6 @@ fx_version('cerulean')
 game('gta5')
 lua54 'on'
 
-client_scripts({
-    'client/main.lua'
-})
-
 files({
     'data/**/carcols.meta',
     'data/**/carvariations.meta',
@@ -21,7 +17,5 @@ data_file('VEHICLE_METADATA_FILE')('data/**/vehicles.meta')
 data_file('CARCOLS_FILE')('data/**/carcols.meta')
 data_file('VEHICLE_VARIATION_FILE')('data/**/carvariations.meta')
 data_file('VEHICLE_LAYOUTS_FILE')('data/**/vehiclelayouts.meta')
-
-dependency '/assetpacks'
 
 --Made by @Bordless#7020 for FiveM
